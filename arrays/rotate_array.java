5b2d208ae0cde9ed09482e095d2d3be576b4bdfe
ansh271 <anshgeu27@gmail.com>
2025-08-27 18:44:00 +0530

    TwoSum Leet code solution

--- arrays/rotate_array.java
+++
@@ -1,5 +0,0 @@
-package arrays;
-
-public class rotate_array {
-    
-}
--- arrays/twosum.java
+++ arrays/twosum.java
@@ -1,29 +1,15 @@
-/*gt
- Problem: https://leetcode.com/problems/two-sum/
- Approach: HashMap (O(n))
- Date: 26-08-2025
-*/
-
-import java.util.*;
-
-class TwoSum {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+        int[] arr = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    arr[0] = i;
+                    arr[1] = j;
+                    break;
+                }
             }
-            map.put(nums[i], i);
         }
-        return new int[]{}; // No solution found
-    }
-
-    public static void main(String[] args) {
-        TwoSum obj = new TwoSum();
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        int[] result = obj.twoSum(nums, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+        return arr;
     }
 }
